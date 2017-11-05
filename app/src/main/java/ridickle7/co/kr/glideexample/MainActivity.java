abf50849f9d592304072aa11c3465164f38ef5a2
ridickle <ridickle7@gmail.com>
2017-11-06 01:37:31 +0900

    Init Glide Project

--- app/src/main/java/ridickle7/co/kr/glideexample/MainActivity.java
+++ app/src/main/java/ridickle7/co/kr/glideexample/MainActivity.java
@@ -1,15 +1,22 @@
 package ridickle7.co.kr.glideexample;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView iv1, iv2;
+    Button bu1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +25,34 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        bu1 = (Button) findViewById(R.id.defaultURLCallButton);
+        iv1 = (ImageView) findViewById(R.id.defaultURLCallIv);
+        iv2 = (ImageView) findViewById(R.id.GIFIv);
+
+        // 1. Drawable 데이터를 얻어 처리하는 방법
+        bu1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // 1. 로드된 이미지를 받을 Target 생성 (인수에 width, height 설정 가능)
+                SimpleTarget target = new SimpleTarget<Drawable>(250, 250) {
+                    @Override
+                    public void onResourceReady(Drawable resource, Transition<? super Drawable> transition) {
+                        //TODO:: 리소스 로드가 끝난 후 수행할 작업
+                        iv1.setBackground(resource);
+                    }
+                };
+
+                // 2. Glide를 활용한다.
+                GlideApp.with(getApplicationContext())
+                        .load("https://cdn.rideapart.com/wp-content/uploads/2016/04/HD6.jpg")
+                        .placeholder(R.mipmap.ic_launcher)
+                        .error(R.mipmap.ic_launcher_round)
+                        .into(target);
             }
         });
+
+        //2. 이미지뷰에 직접 URL 이미지를 넣는 방법
+        Glide.with(this).load("https://github.com/bumptech/glide/raw/master/static/glide_logo.png").into(iv2);
     }
 
     @Override
---
+++ app/src/main/java/ridickle7/co/kr/glideexample/MyAppGlideModule.java
@@ -0,0 +1,13 @@
+package ridickle7.co.kr.glideexample;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by ridickle on 2017. 11. 5..
+ */
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {
+}
+
